afa24a0197a50c7b3528cbbd48998af0dd179273
Vloods <44786175+Vloods@users.noreply.github.com>
2019-06-27 01:07:11 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,5 @@
 from fastai.vision import *
 
-# В данном классе мы хотим полностью производить всю обработку картинок, которые поступают к нам из телеграма.
-# Это всего лишь заготовка, поэтому не стесняйтесь менять имена функций, добавлять аргументы, свои классы и
-# все такое.
 def load_model():
     learn = load_learner('model')
     return learn
@@ -10,14 +7,28 @@ def load_model():
 class ArtPredictor:
     def __init__(self):
         self.model = load_model()
-
+        self.labels = ['Albrecht Dürer', 'Alfred Sisley', 'Amedeo Modigliani', 'Andrei Rublev', 'Andy Warhol',
+               'Camille Pissarro', 'Caravaggio',
+               'Claude Monet', 'Diego Rivera', 'Diego Velazquez', 'Edgar Degas', 'Edouard Manet', 'Edvard Munch',
+               'El Greco', 'Eugene Delacroix',
+               'Francisco Goya', 'Frida Kahlo', 'Georges Seurat', 'Giotto di Bondone', 'Gustav Klimt',
+               'Gustave Courbet', 'Henri Matisse', 'Henri Rousseau',
+               'Henri de Toulouse-Lautrec', 'Hieronymus Bosch', 'Jackson Pollock', 'Jan van Eyck', 'Joan Miro',
+               'Kazimir Malevich', 'Leonardo da Vinci',
+               'Marc Chagall', 'Michelangelo', 'Mikhail Vrubel', 'Pablo Picasso', 'Paul Cezanne', 'Paul Gauguin',
+               'Paul Klee', 'Peter Paul Rubens', 'Pierre-Auguste Renoir',
+               'Piet Mondrian', 'Pieter Bruegel', 'Raphael', 'Rembrandt', 'Rene Magritte', 'Salvador Dali',
+               'Sandro Botticelli', 'Titian', 'Vasiliy Kandinskiy', 'Vincent van Gogh', 'William Turner']
     def predict(self, stream):
         img = (open_image(stream))
         img = img.resize(229)
         img.refresh()
-        pred, percent= self.evaluate_image(img)
-        return pred, percent
+        percent= self.evaluate_image(img)
+        preds = zip(self.labels, percent)
+        preds = sorted(preds, key = lambda t: t[1].item(), reverse=True)
+        preds = [list(t) for t in zip(*kek)]
+        return preds
 
     def evaluate_image(self, img)->(str, float):
         pred_class, pred_idx, outputs = self.model.predict(img)
-        return pred_class, outputs.max()*100
+        return outputs*100
